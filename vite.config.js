7e1e4c23ac765c7b1f3dbf749f6d4eadda207ff5
liangjianhao022 <3451702346@qq.com>
2025-12-30 23:08:51 +0800

    Update vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ljh20251230/', // 将 【'your-repo-name'】 替换为你的 GitHub 仓库名，结尾的斜杠是必须的。
+  base: '/liangjianhao20251230/',
   plugins: [vue()],
 })
